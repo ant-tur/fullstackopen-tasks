3563a34a114674348507daef40e77661e7a6bd41
Anton Tur <ant.tur85@gmail.com>
2025-07-30 22:26:17 +0300

    add solutions 2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,30 +4,34 @@ import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import personsService from './services/persons';
+import SuccessMessage from './components/SuccessMessage';
+import ErrorMessage from './components/ErrorMessage';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [filterField, setFilterField] = useState('');
+  const [message, setMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const { getAll, createPerson, updatePerson, deletePerson } = personsService;
 
   useEffect(() => {
-    getAll().then((initialData) => {
+    getAll().then(initialData => {
       setPersons(initialData);
     });
   }, []);
 
-  const handlerNameChange = (event) => {
+  const handlerNameChange = event => {
     setNewName(event.target.value);
   };
 
-  const handlerPhoneChange = (event) => {
+  const handlerPhoneChange = event => {
     setPhoneNumber(event.target.value);
   };
 
-  const addNewName = (event) => {
+  const addNewName = event => {
     event.preventDefault();
 
     const person = {
@@ -35,9 +39,14 @@ const App = () => {
       number: phoneNumber,
     };
 
-    if (persons.every((p) => p.name !== newName)) {
-      createPerson(person).then((returnedPerson) => {
+    if (persons.every(p => p.name !== newName)) {
+      createPerson(person).then(returnedPerson => {
+        setMessage(`Adding ${returnedPerson.name}`);
         setPersons(persons.concat(returnedPerson));
+
+        setTimeout(() => {
+          setMessage(null);
+        }, 3000);
       });
     } else {
       if (
@@ -45,13 +54,30 @@ const App = () => {
           `${newName} is already added to phonebook, replace the old number with a new one?`
         )
       ) {
-        const oldPerson = persons.find((p) => p.name === newName);
+        const oldPerson = persons.find(p => p.name === newName);
 
-        updatePerson(oldPerson.id, person).then((returnedPerson) => {
-          setPersons(
-            persons.map((p) => (p.id === oldPerson.id ? returnedPerson : p))
-          );
-        });
+        updatePerson(oldPerson.id, person)
+          .then(returnedPerson => {
+            setMessage(`Updated phone number of ${returnedPerson.name}`);
+            setPersons(
+              persons.map(p => (p.id === oldPerson.id ? returnedPerson : p))
+            );
+
+            // setTimeout(() => {
+            //   setMessage(null);
+            // }, 3000);
+          })
+          .catch(() => {
+            setErrorMessage(
+              `Information of ${person.name} has already been removed from server`
+            );
+          })
+          .finally(() => {
+            setTimeout(() => {
+              setMessage(null);
+              setErrorMessage(null);
+            }, 3000);
+          });
       }
     }
 
@@ -59,23 +85,36 @@ const App = () => {
     setPhoneNumber('');
   };
 
-  const removePerson = (id) => {
-    const person = persons.find((p) => p.id === id);
+  const removePerson = id => {
+    const person = persons.find(p => p.id === id);
 
     if (window.confirm(`Delete ${person.name}?`)) {
-      deletePerson(id).then(() => {
-        setPersons(persons.filter((person) => person.id !== id));
-      });
+      deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id));
+        })
+        .catch(() => {
+          setErrorMessage(
+            `Information of ${person.name} has already been removed from server`
+          );
+        })
+        .finally(() => {
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 3000);
+        });
     }
   };
 
-  const filteredPersons = persons.filter((person) =>
+  const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(filterField.toLowerCase())
   );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <SuccessMessage message={message} />
+      <ErrorMessage message={errorMessage} />
       <Filter filterField={filterField} setFilterField={setFilterField} />
 
       <h3>add a new</h3>
