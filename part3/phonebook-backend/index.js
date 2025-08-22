d799a3d4e45ef7ece8d1fd76d578e4f520046676
Anton Tur <ant.tur85@gmail.com>
2025-08-22 17:22:18 +0300

    add solutions 3.15-3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -41,39 +41,48 @@ let persons = [
   },
 ];
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons);
-  });
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons);
+    })
+    .catch(error => next(error));
 });
 
-app.get('/api/info', (request, response) => {
-  Person.find({}).then(persons => {
-    response.send(
-      `<p>Phonebook has info for ${persons.length} people</p>
-      <p>${new Date()}</p>`
-    );
-  });
+app.get('/api/info', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>`
+      );
+    })
+    .catch(error => next(error));
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter(pers => pers.id !== id);
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 
   response.status(204).end();
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -93,10 +102,72 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   });
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson);
+  person
+    .save()
+    .then(savedPerson => {
+      response.json(savedPerson);
+    })
+    .catch(error => next(error));
+});
+
+// app.put('/api/persons/:id', (request, response, next) => {
+//   const { name, number } = request.body;
+
+//   Person.findByIdAndUpdate(
+//     request.params.id,
+//     { name, number },
+//     {
+//       new: true,
+//       runValidators: true,
+//       context: 'query',
+//     }
+//   )
+//     .then(updatedPerson => {
+//       if (!updatedPerson) {
+//         return response.status(404).json({ error: 'person not found' });
+//       }
+
+//       response.json(updatedPerson);
+//     })
+//     .catch(error => next(error));
+// });
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
+
+  Person.findById(request.params.id).then(person => {
+    if (!person) {
+      return response.status(404).json({ error: 'person not found' });
+    }
+
+    person.number = number;
+
+    return person
+      .save()
+      .then(updatedPerson => {
+        response.json(updatedPerson);
+      })
+      .catch(error => next(error));
   });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformed id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
