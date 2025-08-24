2d87fb7126396f31db61f5ec843d84496127bee0
Anton Tur <ant.tur85@gmail.com>
2025-08-24 19:33:37 +0300

    add solution 3.22

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -18,29 +18,6 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then(persons => {
@@ -129,7 +106,7 @@ app.post('/api/persons', (request, response, next) => {
 // });
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const { name, number } = request.body;
+  const { number } = request.body;
 
   Person.findById(request.params.id).then(person => {
     if (!person) {
